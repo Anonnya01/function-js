6e787487afbfa85bf27d64dbb04ec70abf0119ba
Your Name <you@example.com>
2025-08-07 23:27:28 +0600

    added return

---
+++ parameter.js
@@ -0,0 +1,25 @@
+// function square(num){
+//  console.log('Value',num);
+//  const number = num * num;
+//  console.log('answer', number);
+ 
+// }
+
+// square(4);
+// square(5);
+
+// const value =10;
+// square(value);
+
+// subtraction-----------------
+
+function sub(x , y) {
+    const minus = x - y;
+    console.log(x ,y , "answer",minus);
+    
+}
+
+const a = 33;
+const b = 20;
+
+sub(a,b);
---
+++ return.js
@@ -0,0 +1,16 @@
+function tenTime(num){
+    const result = num * 10;
+    return result;
+  
+    
+}
+
+
+function half(num){
+    const half = num / 2;
+    return half;
+}
+
+tenTime(5);
+const output = tenTime(5);
+console.log(output);
