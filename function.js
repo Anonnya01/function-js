31d586820ad6036de8475f3ef0c5340dae0ef484
Your Name <you@example.com>
2025-08-07 22:07:23 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,45 @@
+// function hello() {
+//     let sum = 2 +5;
+//     console.log(sum);
+
+// }
+// hello();
+
+// function money(){
+//     let money = 200;
+//     console.log(" I have " + money  + "Tk");
+
+// }
+// money();
+
+// =======add
+
+// function calc(input1, input2) {
+//   let sum = input1 - input2;
+//   console.log("Sum is =" + sum);
+//   return sum;
+// }
+// const result = calc(22, 10);
+// console.log(result);
+
+
+// function name( x , y) {
+//     const result = x + y;
+//     return result;
+// }
+// const sum= name(2 ,4);
+//     console.log(sum);
+
+
+// example//
+
+
+function pasta( sauce ,spice ,cheese){
+    console.log('Boil water');
+    console.log('Add spices');
+    console.log('Add meats');
+    console.log(sauce + ' add' );
+     return "Tasty pasta"
+}
+const result = pasta('cream', 'pepper' , 'parmesan')
+console.log(result);
