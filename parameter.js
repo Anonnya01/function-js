ab10b9cb453430309fec914c4025da24dd7e108f
Your Name <you@example.com>
2025-08-08 00:17:54 +0600

    updated

--- parameter.js
+++ parameter.js
@@ -13,13 +13,59 @@
 
 // subtraction-----------------
 
-function sub(x , y) {
-    const minus = x - y;
-    console.log(x ,y , "answer",minus);
+// function sub(x , y) {
+//     const minus = x - y;
+//     console.log(x ,y , "answer",minus);
     
+// }
+
+// const a = 33;
+// const b = 20;
+
+// sub(a,b);
+
+
+
+// string
+// function sizeS(str) {
+//     const size =str.length;
+//     if(size % 2 === 0){
+//         console.log('even');
+//     return true;}
+//   else{
+//     console.log('odd');
+//     return false
+//   }
+//   console.log(str);
+
+// }
+      
+
+// sizeS('Simba')
+// sizeS('Anonnya')
+
+
+
+// function dOrt(number, double) {
+//     if(double === true){
+//         const result = number *2;
+//         return result;
+//     }
+//     else{
+//         const result = number *3;
+//         return result;
+//     }
+// }
+
+
+// console.log(dOrt(5,true))
+// console.log(dOrt(5,false))
+
+
+function elements(num) {
+    const len = num.length;
+    return len;
 }
 
-const a = 33;
-const b = 20;
-
-sub(a,b);
+elements([12,11, 6, 4])
+console.log(elements([12,11, 6, 4]));
--- return.js
+++ return.js
@@ -1,16 +1,69 @@
-function tenTime(num){
-    const result = num * 10;
-    return result;
-  
-    
-}
+// function tenTime(num){
+//     const result = num * 10;
+//     return result;
 
+// }
 
-function half(num){
-    const half = num / 2;
-    return half;
-}
+// function half(num){
+//     const half = num / 2;
+//     return half;
+// }
 
-tenTime(5);
-const output = tenTime(5);
-console.log(output);
+// tenTime(5);
+// const output = tenTime(5);
+// console.log(output);
+
+// function add(x , y){
+// const total = x + y;
+// return total;
+
+// }
+
+// const bill = add(40 , 30);
+// console.log(bill)
+
+// function add2( a ,b){
+//    return a + b;
+// }
+// const bill2 = add(22,5);
+// console.log(bill,bill2);
+
+// complex
+
+// function math(n1 , n2){
+//     const sum = n1 + n2;
+//     const sub = n1 - n2;
+//     const multiply = sum * sub;
+//     const result = multiply / 2;
+//     return result;
+// }
+
+// const result = math( 20 ,10);
+// console.log(result);
+
+// // odd even
+
+// function isOdd(number) {
+//   if ((b = number % 2 === 1)) {
+//     return true;
+//   }
+//   // else{
+//   //     return false;
+//   // }
+//   return false;
+// }
+
+// console.log(isOdd(7));
+// console.log(isOdd(10));
+
+// function isEven(number){
+//     if(b=number % 2 ===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(isEven(7));
+// console.log(isEven(10));
